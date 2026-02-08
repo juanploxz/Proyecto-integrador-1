42031019665a221cb9bf0c4e8b0ed1fcf96c4e67
juanploxz <juanploxz@gmail.com>
2026-02-07 23:27:02 -0500

    ajuste de nombre

--- movie/views.py
+++ movie/views.py
@@ -12,7 +12,8 @@ def home(request):
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else:
         movies = Movie.objects.all()
-    return render(request, 'home.html', {searchTerm: searchTerm, 'movies': movies})
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies, 'name':'Juan Pablo Parra'})
+
 
 
 def about(request):
